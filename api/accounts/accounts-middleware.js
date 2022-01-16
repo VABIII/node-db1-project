697600eae16c95a80370afc86b14c42aa222a2c0
VABIII <tbengtson82@gmail.com>
2022-01-15 22:45:24 -0600

    re-doing middlewares

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,53 +1,26 @@
-const yup = require('yup')
 const Accounts = require('./accounts-model')
 const db = require('../../data/db-config')
 
-const accountsSchema = yup.object({
-    name: yup.string().trim().min(3).max(100).required(),
-    budget: yup.number().min(0).max(1000000).required()
-})
+.exports.checkAccountPayload = (req, res, next) => {
+    const error = {status: 400}
+    const { name, budget } = req.body
 
-
-
-
-exports.checkAccountPayload = async (req, res, next) => {
-    const {name, budget} = req.body
-
-    if(!name || !budget) {
-        next({status: 400, message:"Suck it Trebec!"})
+    if(name === undefined || budget === undefined) {
+        error.message = 'name and budget are required'
     } else if (name.trim().length < 3 || name.trim().length > 100) {
-        next({status:400, message: "Too long/not long enough"})
-    } else if (isNaN(parseInt(budget))) {
-        next({status: 400, message: "Budget must be a number"})
-    } else if (budget < 0 || budget > 1000000) {
-        next({status: 404, message: "Budget is fucked"} )
+        error.message = 'name of account must be between 3 and 100'
+    } else if (typeof parseInt(budget) !== 'number') {
+        error.message = 'budget of account must be a number'
+    } else if (budget < 0 || budget > 100000) {
+        error.message = 'budget of account is too large or too small'
     }
 
-    else {next()}
+    if(error.message) {
+        next(error)
+    } else {next()}
 }
 
 
-exports.checkAccountNameUnique = (req, res, next) => {
-
-    try {
-        const account =  db('accounts').where('name', req.body.name.trim()).first()
-        if(!account) {
-            next({
-                status: 400,
-                message: 'That name already exists'
-            })
-        } else {next()}
-
-    }
-    catch(err) {
-        next(err)
-    }
-
-}
-
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
 
 exports.logger = (req, res, next) => {
     console.log(`[${req.method}]  ${req.url} ${new Date()}`)
